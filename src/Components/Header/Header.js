e7daa7fe4e0d277f40278caebd259a9d94b88830
subhadeepdhar17 <oliverdepp17@gmail.com>
2023-08-29 10:34:37 +0530

    Map function for header menu

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,7 +1,7 @@
 import React, { useState} from 'react';
 import './Header.scss';
 import Logo from '../../icons_assets/Logo.svg';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 
@@ -10,31 +10,34 @@ const Header = () => {
 
   const [menuDisplay, setMenuDisplay] = useState(false);
 
+  const Menu = [
+    {
+      name: 'Home',
+      to: '/'
+    },
+    {
+      name: 'Menu',
+      to: 'menu'
+    },
+    {
+      name: 'Chefs',
+      to: '/chefs'
+    },
+  ]
+  const MenuDisplay = Menu.map((list) => <li><NavLink to={list.to}>{list.name}</NavLink></li>);
+
   return (
     <>
       <ul>
         <li>
-          <div className='hamburger' onClick={() => {
-            setMenuDisplay(!menuDisplay)
-          }}>
-            <button>==</button>
-          </div>
-        </li>
-        <li className='logo'>
-          <NavLink to="/">
-            <img src={Logo}/>
-          </NavLink>
+          <button>Hamberger</button>
         </li>
         <li>
-          <div className={`menu menuDisplay ${menuDisplay ? 'active' : 'inactive'}`}>
-            <li><NavLink to="/">Home</NavLink></li>
-            <li><NavLink to="menu">Menu</NavLink></li>
-            <li><NavLink to="chefs">Chefs</NavLink></li>
-            <li><NavLink to="specials">Specials</NavLink></li>
-          </div>
+          <img src={Logo} />
         </li>
+        <li>{MenuDisplay}</li>
         <li>
-          <button>|||</button>
+          <button>Basket</button>
         </li>
       </ul>
     </>
@@ -42,3 +45,30 @@ const Header = () => {
 }
 
 export default Header;
+
+/* <ul>
+    <li>
+      <div className='hamburger' onClick={() => {
+        setMenuDisplay(!menuDisplay)
+      }}>
+        <button>==</button>
+      </div>
+    </li>
+    <li className='logo'>
+      <NavLink to="/">
+        <img src={Logo}/>
+      </NavLink>
+    </li>
+    <li>
+      <div className={`menu menuDisplay ${menuDisplay ? 'active' : 'inactive'}`}>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="menu">Menu</NavLink></li>
+        <li><NavLink to="chefs">Chefs</NavLink></li>
+        <li><NavLink to="specials">Specials</NavLink></li>
+      </div>
+    </li>
+    <li>
+      <button>|||</button>
+    </li>
+  </ul>
+*/
