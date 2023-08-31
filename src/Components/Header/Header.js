95be1cd3e9640f276c879beee69729665c5ee8f9
subhadeepdhar17 <oliverdepp17@gmail.com>
2023-08-31 20:15:33 +0530

    Updated Mobile Header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,6 +2,8 @@ import React, { useState} from 'react';
 import './Header.scss';
 import Logo from '../../icons_assets/Logo.svg';
 import { NavLink } from 'react-router-dom';
+import Hamburger from '../../icons_assets/🦆 icon _hamburger menu.svg';
+import Basket from '../../icons_assets/Basket.svg';
 
 const Header = () => {
 
@@ -26,7 +28,7 @@ const Header = () => {
     },
     {
       name: 'Book A Table',
-      to: '/specials'
+      to: '/reservations'
     },
   ]
   const MenuDisplay = Menu.map((list) => <li><NavLink to={list.to}>{list.name}</NavLink></li>);
@@ -34,17 +36,21 @@ const Header = () => {
   return (
     <>
       <ul>
-        <li onClick={() => {setMenuDisplay(!menuDisplay)}}>
-          <button className='hamburger'>Hamburger</button>
+        <li className='hamburgerLI' onClick={() => {setMenuDisplay(!menuDisplay)}}>
+          <div className={`hamburger ${menuDisplay ? 'active' : 'inactive'}`}>
+            <img src={Hamburger} />
+          </div>
         </li>
         <li className='logo'>
-          <NavLink to='/'><img src={Logo} alt='' /></NavLink>
+          <NavLink to='/'><img src={Logo} alt='' onClick={() => setMenuDisplay(false)}/></NavLink>
         </li>
         <li className='computerMenu'>
           {MenuDisplay}
         </li>
         <li>
-          <button className='basket'>Basket</button>
+          <div className='basket'>
+            <img src={Basket} />
+          </div>
         </li>
       </ul>
       <div className={`mobileMenu ${menuDisplay ? 'active' : 'inactive'}`} onClick={() => setMenuDisplay(false)}>
